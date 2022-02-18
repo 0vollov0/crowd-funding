3869c3160723daf0f1e62eda7f96489f12f9cbf2
park singyu <psk876@naver.com>
2022-02-18 23:00:43 +0900

    rename, fix bug, add some modifier and add funding open state

--- CrowdFundingFactory.sol
+++ CrowdFundingFactory.sol
@@ -1,15 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-
 contract CrowdFundingFactory {
     using SafeMath for uint256;
 
     event CreateFunding(uint id, string title, address owner);
 
-    // It will check whether funding beginning time is past now
-    modifier fundingBeginMustBePassedNow(uint _fundingBegin) {
-        require(block.timestamp <= _fundingBegin);
+    uint coinUnit = 1 wei;
+
+    modifier beginFunding(uint _fundingId) {
+        require(block.timestamp <= fundings[_fundingId].beginTime);
+        _;
+    }
+
+    modifier endFunding(uint _fundingId) {
+        require(fundings[_fundingId].endTime < block.timestamp);
+        _;
+    }
+
+    modifier availableFunding(uint _fundingId) {
+        uint time = block.timestamp;
+        require(fundings[_fundingId].beginTime < time,"The funding not open yet.");
+        require(time < fundings[_fundingId].endTime,"The funding closed.");
+        _;
+    }
+
+    modifier ownerOfFunding(uint _fundingId) {
+        require(msg.sender == fundings[_fundingId].fundraiser);
+        _;
+    }
+
+    modifier availableAmount(uint _fundingId) {
+        require(fundings[_fundingId].availableMinAmount * coinUnit <= msg.value);
         _;
     }
 
@@ -20,9 +42,10 @@ contract CrowdFundingFactory {
         uint currentAmount;
         uint availableMinAmount;
         uint goalAmount;
-        uint fundingBegin;
-        uint fundingEnd;
+        uint beginTime;
+        uint endTime;
         address fundraiser;
+        bool open;
     }
 
     Funding[] public fundings;
@@ -36,12 +59,13 @@ contract CrowdFundingFactory {
         string memory _comments,
         uint _goalAmount,
         uint _availableMinAmount,
-        uint _fundingBegin,
-        uint _fundingEnd) external returns(uint) {
+        uint _beginTime,
+        uint _endTime) external returns(uint) {
         fundings.push(
-            Funding(_title, _subTitle, _comments, 0, _availableMinAmount, _goalAmount, _fundingBegin, _fundingEnd, msg.sender)
+            Funding(_title, _subTitle, _comments, 0, _availableMinAmount * coinUnit, _goalAmount, _beginTime, _endTime, msg.sender, true)
         );
-
+        // require(block.timestamp < _beginTime, "The beginTime must be after block timestamp.");
+        require(_beginTime < _endTime, "The beginTime must be before the endTime.");
         uint id = fundings.length.sub(1);
         fundingToFundraiser[id] = msg.sender;
         addressToFundingIds[msg.sender].push(id);
@@ -52,7 +76,7 @@ contract CrowdFundingFactory {
     function getMyFundingList() external view returns(Funding [] memory) {
         uint[] memory fundingIds = addressToFundingIds[msg.sender];
         Funding[] memory myFundings = new Funding[](fundingIds.length);
-        for(uint i=0; i < fundingIds.length; i.add(1)) {
+        for(uint i=0; i < fundingIds.length; i++) {
             myFundings[i] = fundings[fundingIds[i]];
         }
         return myFundings;
