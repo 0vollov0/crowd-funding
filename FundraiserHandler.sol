bba3400bd4b9f74923bdb71bf1023e4291a810fb
park singyu <psk876@naver.com>
2022-02-21 16:45:24 +0900

    modify duplicate fnc name remove useless and update inheritance

--- FundraiserHandler.sol
+++ FundraiserHandler.sol
@@ -1,26 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "./CrowdFundingFactory.sol";
-import "./FundingCoinManager.sol";
+import "./FundingHandler.sol";
 
-contract FundraiserHandler is CrowdFundingFactory, FundingCoinManager {
+contract FundraiserHandler is FundingHandler {
     using SafeMath for uint256;
-
-    function setFudingOpenState(uint _fundingId, bool _flag) external ownerOfFunding(_fundingId) {
-        if (fundings[_fundingId].open != _flag) fundings[_fundingId].open = _flag;
-    }
 
     function setAvailableMinAmount(uint _fundingId, uint _availableMinAmount) external ownerOfFunding(_fundingId) {
         fundings[_fundingId].availableMinAmount = _availableMinAmount;
     }
 
-    function withdraw(address payable _address, uint _fundingId) external {
-        require(fundings[_fundingId].fundraiser == msg.sender);
-        require(fundings[_fundingId].goalAmount <= fundings[_fundingId].currentAmount);
-        require(fundings[_fundingId].endTime <= block.timestamp);
-        _address.transfer(fundings[_fundingId].currentAmount);
-        fundings[_fundingId].currentAmount = 0;
-        fundings[_fundingId].open = false;
+    function withdrawAsFundraiser(address payable _address, uint _fundingId) external fundingSuccessed(_fundingId) {
+        bool success = _transfer(_address, fundings[_fundingId].currentAmount);
+        if(success) fundings[_fundingId].currentAmount = 0;
     }
 }
