12f96a4df5188f63ac1a3d23fc5ab78da06d1b21
park singyu <psk876@naver.com>
2022-02-21 16:43:06 +0900

    add return value

--- FundingCoinManager.sol
+++ FundingCoinManager.sol
@@ -2,9 +2,10 @@
 pragma solidity 0.8.9;
 
 contract FundingCoinManager {
-  function _transfer(address payable _address, uint _amount) internal {
+  function _transfer(address payable _address, uint _amount) internal returns(bool success) {
       require(_address == msg.sender);
       (bool sent, ) = _address.call{value: _amount}("");
       require(sent, "Failed to send Ether");
+      return sent;
   }
 }
