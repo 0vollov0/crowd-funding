8b230bc856a653316d47a920a7755f9834a92152
0vollov0 <psk876@naver.cm>
2022-02-28 03:03:28 -0800

    truffle init and compile

--- contracts/FundingCoinManager.sol
+++ contracts/FundingCoinManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.9;
+pragma solidity 0.8.11;
 
 contract FundingCoinManager {
   function _transfer(address payable _address, uint _amount) internal returns(bool success) {
