3689448104e96212bd21036b0e0d5ebeff823392
0vollov0 <psk876@naver.cm>
2022-03-02 21:04:05 +0900

    update withdraw logic

--- contracts/FundraiserHandler.sol
+++ contracts/FundraiserHandler.sol
@@ -8,11 +8,13 @@ contract FundraiserHandler is FundingHandler {
     using SafeMath for uint256;
 
     function setAvailableMinAmount(uint _fundingId, uint _availableMinAmount) external ownerOfFunding(_fundingId) {
+        require(_availableMinAmount > 0,"the availableMinAmount value must be upper than 0 wei.");
         fundings[_fundingId].availableMinAmount = _availableMinAmount;
     }
 
-    function withdrawAsFundraiser(address payable _address, uint _fundingId) external fundingSucceeded(_fundingId) {
-        bool success = _transfer(_address, fundings[_fundingId].currentAmount);
+    function withdrawAsFundraiser(uint _fundingId) external fundingSucceeded(_fundingId) {
+        bool success = withdraw(fundings[_fundingId].currentAmount);
+        require(success,"withdraw method doesn't work well.");
         if(success) fundings[_fundingId].currentAmount = 0;
     }
 }
