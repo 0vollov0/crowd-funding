f3662f6717f599ceb619cef4d4f638d2758b1e57
0vollov0 <psk876@naver.cm>
2022-03-02 21:02:34 +0900

    remove useless

--- contracts/FundingCoinManager.sol
+++ contracts/FundingCoinManager.sol
@@ -2,13 +2,6 @@
 pragma solidity 0.8.11;
 
 contract FundingCoinManager {
-  function _transfer(address payable _address, uint _amount) internal returns(bool success) {
-      require(_address == msg.sender);
-      (bool sent, ) = _address.call{value: _amount}("");
-      require(sent, "Failed to send Ether");
-      return sent;
-  }
-
   function withdraw(uint _amount) internal returns(bool) {
     (bool success, ) = msg.sender.call{value: _amount}("");
     return success;
