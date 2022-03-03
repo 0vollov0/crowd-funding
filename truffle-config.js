a27875cffce3ec6f89c43b5fd808b54fd06af946
0vollov0 <psk876@naver.cm>
2022-03-03 14:02:07 +0900

    add ropsten test net

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+console.log(mnemonic);
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -39,7 +39,17 @@ module.exports = {
       host: "127.0.0.1",
       port: "7545",
       network_id: "5777",
-    }
+    },
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
 
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
