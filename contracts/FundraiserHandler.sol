84ec824614410d8828eee2ce453398926a2d3cf6
0vollov0 <psk876@naver.cm>
2022-03-05 00:28:08 +0900

    add withdraw event

--- contracts/FundraiserHandler.sol
+++ contracts/FundraiserHandler.sol
@@ -5,6 +5,8 @@ import "./FundingHandler.sol";
 contract FundraiserHandler is FundingHandler {
     using SafeMath for uint256;
 
+    event WithdrawAsFundraiser(address fundraiser, uint amount, uint time);
+
     function setAvailableMinAmount(uint _fundingId, uint _availableMinAmount) external ownerOfFunding(_fundingId) {
         require(_availableMinAmount > 0,"the availableMinAmount value must be upper than 0 wei.");
         fundings[_fundingId].availableMinAmount = _availableMinAmount;
@@ -13,6 +15,9 @@ contract FundraiserHandler is FundingHandler {
     function withdrawAsFundraiser(uint _fundingId) external fundingSucceeded(_fundingId) {
         bool success = withdraw(fundings[_fundingId].currentAmount);
         require(success,"withdraw method doesn't work well.");
-        if(success) fundings[_fundingId].currentAmount = 0;
+        if(success) {
+            emit WithdrawAsFundraiser(msg.sender, fundings[_fundingId].currentAmount, block.timestamp);
+            fundings[_fundingId].currentAmount = 0;
+        }
     }
 }
