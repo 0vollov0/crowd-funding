c04dfc5575ccb061ddf37e1b5f9684ba2c9ce2cb
0vollov0 <psk876@naver.cm>
2022-03-05 00:29:22 +0900

    change contract address

--- js/CrowdFunding.js
+++ js/CrowdFunding.js
@@ -6,22 +6,22 @@ import Web3 from 'web3';
 import Event from './Event.js';
 
 class CrowdFunding extends User {
-	constructor() {
-		const mnemonic = readFileSync(path.resolve('.secret')).toString().trim();
+  constructor() {
+    const mnemonic = readFileSync(path.resolve('.secret')).toString().trim();
 
-		const account = '0x0602e801fd109DB0Edb8AE8AC92d138ca6cCf668'; // update account to yours.
-		const contract_address = '0xA0A5B8867D21BD4d7fA8119c84D8CB36a84e45f2';
+    const account = '0x0602e801fd109DB0Edb8AE8AC92d138ca6cCf668'; // update account to yours.
+    const contract_address = '0x8732FE7a3D5337836C85A2CDa5D371Ebc5308780';
 
-		const web3 = new Web3(new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161`));
-		const web3_ws = new Web3(new Web3.providers.WebsocketProvider('wss://ropsten.infura.io/ws/v3/9aa3d95b3bc440fa88ea12eaa4456161'));
+    const web3 = new Web3(new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161`));
+    const web3_ws = new Web3(new Web3.providers.WebsocketProvider('wss://ropsten.infura.io/ws/v3/9aa3d95b3bc440fa88ea12eaa4456161'));
 
-		const abi = JSON.parse(readFileSync('./build/contracts/CrowdFunding.json','utf-8')).abi;
-		const contract = new web3.eth.Contract(abi,contract_address);
-		const contract_event = new web3_ws.eth.Contract(abi,contract_address);
+    const abi = JSON.parse(readFileSync('./build/contracts/CrowdFunding.json','utf-8')).abi;
+    const contract = new web3.eth.Contract(abi,contract_address);
+    const contract_event = new web3_ws.eth.Contract(abi,contract_address);
 
     super(contract, account);
-		this.event = new Event(contract_event);
-		this.event.on();
+    this.event = new Event(contract_event);
+    this.event.on();
   }
 }
 
